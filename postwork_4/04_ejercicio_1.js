8914f1da5f7f91a9e68ef5841ca2d427d6edca41
Cru5it0 <cruzisaacarandaervantes@gmail.com>
2022-09-27 11:06:25 -0500

    Ejercicos del postwork 3

---
+++ postwork_4/04_ejercicio_1.js
@@ -0,0 +1,43 @@
+/**
+ * Asynchronous Output
+ * 
+ * Completar la función triggerActions que pasa un callback a 
+ * processAction para producir el siguiente resultado:
+ */
+
+// "Processed Action 1"
+// "Processed Action 2"
+// "Processed Action 3"
+// ...
+// "Processed Action n"
+
+/**
+ * Se debe mostrar el mensaje n cantidad de veces, 
+ * determinado por triggerActions. Tomar en cuenta que no se 
+ * debe modificar la función processAction. 
+ */
+
+
+ // Don't alter this function
+const processAction = (i, callback) => {
+    setTimeout(function() {
+        callback("Processed Action " + i);
+    }, Math.random()*1000);
+}
+
+const triggerActions = (count) => {
+    // Code goes here
+    // Funcion local para procesar y dar el numero de la accion
+    let triggerAction = function (i) { 
+        if (i <= count) {  // Mientras el stop (i) sea mayor o igual al contador continuara haciendo:
+            processAction(i, function (text) { // para llamar un callbak se necesita enviar una funcion que sera el texto a imprimir
+                console.log(text);
+                triggerAction(i + 1); // Dentro de la funcion estara reciviendo el contador y cada vez incrementara
+            });
+        }
+    }
+    triggerAction(1); //Le endicamos a la accion donde iniciara
+}
+
+//Llamar a lafunccion con el limite de mensajes
+triggerActions(10);
